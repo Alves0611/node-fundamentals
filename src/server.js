1f4d6d760d590b6c4a3bdff77d383949317af114
Alves0611 <g.alves0611@gmail.com>
2024-03-16 23:48:54 -0300

    Implemented in-memory storage and JSON response for users

--- src/server.js
+++ src/server.js
@@ -1,13 +1,29 @@
 import http from 'node:http'
 
+// GET => Buscar um recurso no back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar um informação específica de um recurso no back-end
+// DELETE => Deletar um recurso do back-end
+
+const users = []
+
 const server = http.createServer((req, res) => {
     const { method, url} = req 
 
     if(method === 'GET' && url === '/users') {
-        return res.end('Listagem de usuários')
+        return res
+        .setHeader('Content-Type', 'application/json')
+        .end(JSON.stringify(users));
     }
 
     if(method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'Gabriel',
+            email: 'gabriel@example.com'
+        })
+
         return res.end('Criação de usuários')
     }
 
