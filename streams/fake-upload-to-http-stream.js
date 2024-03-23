98c8c51f6108316e622740581e56413985167d86
Alves0611 <g.alves0611@gmail.com>
2024-03-23 10:01:30 -0300

    Consuming a complete stream

--- streams/fake-upload-to-http-stream.js
+++ streams/fake-upload-to-http-stream.js
@@ -1,26 +1,25 @@
 import { Readable } from 'node:stream'
-
 class OneToHundredStream extends Readable {
-    constructor(options) {
-        super(options);
-        this.index = 1;
-    }
+  index = 1
+  _read() {
+    const i = this.index++
 
-    _read() {
-        const i = this.index++;
-
-        setTimeout(() => {
-            if (i > 100) {
-                this.push(null);
-            } else {
-                const buf = Buffer.from(String(i), 'utf-8');
-                this.push(buf);
-            }
-        }, 1000);
-    }
+    setTimeout(() => {
+      if (i > 5) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+        this.push(buf)
+      }
+    }, 1000);
+  }
 }
 
 fetch('http://localhost:3334', {
-    method: 'POST',
-    body: new OneToHundredStream(), 
+  method: 'POST',
+  body: new OneToHundredStream(),
+}).then(res => {
+  return res.text()
+}).then(data => {
+  console.log(data)
 })
--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -1,21 +1,28 @@
-import http from 'node>http'
+import http from 'node:http'
 import { Transform } from 'node:stream'
-
 class InverseNumberStream extends Transform {
-    _transform(chunk, encoding, callback) {
-        const transformed = Number(chunk.toString()) * -1
-
-        callback(null, Buffer.from(String(transformed)))
-    }
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+    console.log(transformed)
+    callback(null, Buffer.from(String(transformed)))
+  }
 }
+// req => ReadableStream
+// res => WritableStream
 
-// req => ReadbleStream
-// res => WritebleStream
 
-const server = http.createServer((req, res) => {
-    return req
-        .pipe(new InverseNumberStream())
-        .pipe(res)
+const server = http.createServer(async (req, res) => {
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString()
+
+  console.log(fullStreamContent)
+
+  return res.end(fullStreamContent)
 })
 
 server.listen(3334)
